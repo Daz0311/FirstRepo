0a8fbe8cf707bddc8f7d8b1990c8d49a31f77079
DT-08\Admin <dario.tarasco@remira.com>
2024-07-30 14:23:14 +0200

    commit con errore da risolvere

--- DbContexts/CityInfoContext.cs
+++ DbContexts/CityInfoContext.cs
@@ -8,15 +8,17 @@ namespace CityInfo.API.DbContexts
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointOfInterests { get; set; }
 
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+        }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("connectionstring");
 
             base.OnConfiguring(optionsBuilder);
-        }
-        public CityInfoContext(DbContextOptions<CityInfoContext> options)
-      : base(options)
-        {
         }
 
     }
