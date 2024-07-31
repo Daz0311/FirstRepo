0376b72a1b8b15ad47bfd1ef6982a45afef0b303
DT-08\Admin <dario.tarasco@remira.com>
2024-07-31 09:56:42 +0200

    First Commit

--- obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8fbe8cf707bddc8f7d8b1990c8d49a31f77079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efb3fc34e5da3c60cf67b26c9b3e2ff37aa18d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
